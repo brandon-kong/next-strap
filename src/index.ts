35081cbc0e8ae9561ccbcd09e7eea8053cfd7c82
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-04-13 22:07:32 -0500

    Removed shell

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from "@commander-js/extra-typings";
 import { version } from "../package.json";
 import { checkOutdatedPackage } from "./helpers/util/checkNewPackages";
